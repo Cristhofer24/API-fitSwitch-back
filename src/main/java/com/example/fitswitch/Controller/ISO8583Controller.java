f31a2a97005f1ca0d07cb8717b8f3987dc30d858
karolinagit3007 <karolina.17hidalgo@gmail.com>
2024-12-30 16:46:00 -0500

    configuraciones para mensajes de error y nulos de la tabla y semáforo

--- src/main/java/com/example/fitswitch/Controller/ISO8583Controller.java
+++ src/main/java/com/example/fitswitch/Controller/ISO8583Controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
@@ -19,18 +20,44 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/iso-8583")
 public class ISO8583Controller {
 
+
+
     private final ISO8583Repository iso8583Repository;
 
     public ISO8583Controller(ISO8583Repository iso8583Repository) {
         this.iso8583Repository = iso8583Repository;
     }
 
-    // Obtener todos los registros
+
+    ////Obtener todos los campos y además de eso envía un mensaje a la
+    //// tabla cuando los campos esten vacios o sean nulos.
     @GetMapping
     public List<ISO8583> listarISO8583() {
-        return iso8583Repository.findAll();
+        List<ISO8583> registros = iso8583Repository.findAll();
+
+        for (ISO8583 registro : registros) {
+            // Verificar el valor de y modifica por un mensaje
+            if (registro.getWiso039ResponseCode() != null && !"000".equals(registro.getWiso039ResponseCode())) {
+                registro.setWiso039pRespondeDetail("TRANSACCIÓN FALLIDA");
+            }
+
+            // Reemplazar los campos nulos por el mensaje "Sin registro"
+            for (Field field : ISO8583.class.getDeclaredFields()) {
+                try {
+                    field.setAccessible(true);
+                    if (field.get(registro) == null) {
+                        field.set(registro, "Sin registro");
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return registros;
     }
 
+    ////Permite filtrar información a traves de inputs de la tabla
     @GetMapping("/filtrar")
     public List<ISO8583> filtrarPorFechas(
             @RequestParam("WISO_012_LOCALDATETIME")
@@ -42,8 +69,7 @@ public class ISO8583Controller {
         return iso8583Repository.findByFechaRange(wiso012LocalDateTime, wiso015SettlementDatel);
     }
 
-
-
+    ////Permite poner el semaforo de acuerdo a la condición
     @GetMapping("/semaforo")
     public List<Map<String, Object>> getSemaforoData() {
         List<ISO8583> registros = iso8583Repository.findAll();
@@ -55,5 +81,7 @@ public class ISO8583Controller {
             return map;
         }).collect(Collectors.toList());
     }
+
+
 
 }
--- src/main/java/com/example/fitswitch/Model/ISO8583.java
+++ src/main/java/com/example/fitswitch/Model/ISO8583.java
@@ -1,7 +1,10 @@
 package com.example.fitswitch.Model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.Where;
+
 import java.time.LocalDateTime;
 
 
@@ -68,7 +71,7 @@ public class ISO8583 {
     @Column (name = "WISO_013_LOCALDATE")
     private LocalDateTime wiso013LocalDate;
 
-
+    ////getter and setters de campos obligatorios que piden.
     public String getWiso039ResponseCode() {
         return wiso039ResponseCode;
     }
@@ -76,5 +79,55 @@ public class ISO8583 {
     public void setWiso039ResponseCode(String wiso039ResponseCode) {
         this.wiso039ResponseCode = wiso039ResponseCode;
     }
+
+    public String getWiso039pRespondeDetail() {
+        return wiso039pRespondeDetail;
+    }
+
+    public void setWiso039pRespondeDetail(String wiso039pRespondeDetail) {
+        this.wiso039pRespondeDetail = wiso039pRespondeDetail;
+    }
+
+
+    public String getWiso000MessageType() {
+        return wiso000MessageType;
+    }
+
+    public void setWiso000MessageType(String wiso000MessageType) {
+        this.wiso000MessageType = wiso000MessageType;
+    }
+
+    public String getWiso002Pan() {
+        return wiso002Pan;
+    }
+
+    public void setWiso002Pan(String wiso002Pan) {
+        this.wiso002Pan = wiso002Pan;
+    }
+
+    public String getWiso003ProcessingCode() {
+        return wiso003ProcessingCode;
+    }
+
+    public void setWiso003ProcessingCode(String wiso003ProcessingCode) {
+        this.wiso003ProcessingCode = wiso003ProcessingCode;
+    }
+
+    public String getWiso102AccountId1() {
+        return wiso102AccountId1;
+    }
+
+    public void setWiso102AccountId1(String wiso102AccountId1) {
+        this.wiso102AccountId1 = wiso102AccountId1;
+    }
+
+    public String getWiso103AccountId2() {
+        return wiso103AccountId2;
+    }
+
+    public void setWiso103AccountId2(String wiso103AccountId2) {
+        this.wiso103AccountId2 = wiso103AccountId2;
+    }
 }
+
 
